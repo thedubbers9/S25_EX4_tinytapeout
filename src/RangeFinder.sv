f9a5c3c404f1b7e615992cba8c4862b483f77453
Eric Dubberstein <eric@dubberstein>
2025-02-13 14:54:00 -0500

    added rangefinder code

---
+++ src/RangeFinder.sv
@@ -0,0 +1,92 @@
+module RangeFinder #(parameter WIDTH=16) (
+	input logic [WIDTH-1:0] data_in,
+	input logic clock, reset,
+	input logic go, finish,
+	output logic [WIDTH-1:0] range,
+	output logic debug_error
+	
+	);
+	
+	// flop the current seen highest and lowest values.
+	logic [WIDTH-1:0] low_val, high_val;
+
+	assign range = high_val - low_val;
+
+	// state machine ENUM
+	typedef enum logic [1:0] {  
+		IDLE,
+		GO,
+		FINISH,
+		ERROR
+	} state_t;
+
+	state_t current_state, next_state;
+
+
+	always_comb begin : state_transition_logic
+		// default cases to avoid latches.
+		next_state = current_state;
+		debug_error = 0;
+
+		case (current_state)
+			IDLE: begin
+				// It's an error if finish is asserted before go.
+				if (finish) begin
+					next_state = ERROR;
+				end else if (go) begin
+					next_state = GO;
+				end
+			end
+			GO: begin
+				if (finish) begin
+					next_state = FINISH;
+				end
+			end
+			FINISH: begin
+				// wait a cycle before going back to idle
+				next_state = IDLE;
+			end
+			ERROR: begin
+				if (go) begin
+					next_state = GO;
+				end
+
+				// moore output for debug error
+				debug_error = 1;
+			end
+		endcase
+	end
+
+	always_ff @( posedge clock, posedge reset) begin : blockName
+		if (reset) begin
+			current_state <= IDLE;
+			low_val <= 0;
+			high_val <= 0;
+		end else begin
+			current_state <= next_state;
+			case (current_state)
+				
+				IDLE: begin
+					// overwrite the current low and high values when go signal is recieved.
+					if (go) begin
+						low_val <= data_in;
+						high_val <= data_in;
+					end
+				end
+				GO: begin
+					// update the low and high values if appropriate.
+					if (data_in < low_val) begin
+						low_val <= data_in;
+					end else if (data_in > high_val) begin
+						high_val <= data_in;
+					end
+				end
+			endcase
+		end
+	end
+
+	
+	
+     
+
+endmodule : RangeFinder
--- src/chip.sv
+++ src/chip.sv
@@ -6,43 +6,17 @@ module my_chip (
     input logic clock,
     input logic reset // Important: Reset is ACTIVE-HIGH
 );
-    
-    // Basic counter design as an example
     // TODO: remove the counter design and use this module to insert your own design
     // DO NOT change the I/O header of this design
 
-    wire [6:0] led_out;
-    assign io_out[6:0] = led_out;
+    RangeFinder #(parameter WIDTH=10) (
+        .data_in(io_in[9:0]),
+        .clock(clock), .reset(reset),
+        .go(io_in[10]), .finish(io_in[11]),
+        .range(io_out[9:0]),
+        .debug_error(io_out[10])
+	);
 
-    // external clock is 1000Hz, so need 10 bit counter
-    reg [9:0] second_counter;
-    reg [3:0] digit;
-
-    always @(posedge clock) begin
-        // if reset, set counter to 0
-        if (reset) begin
-            second_counter <= 0;
-            digit <= 0;
-        end else begin
-            // if up to 16e6
-            if (second_counter == 1000) begin
-                // reset
-                second_counter <= 0;
-
-                // increment digit
-                digit <= digit + 1'b1;
-
-                // only count from 0 to 9
-                if (digit == 9)
-                    digit <= 0;
-
-            end else
-                // increment counter
-                second_counter <= second_counter + 1'b1;
-        end
-    end
-
-    // instantiate segment display
-    seg7 seg7(.counter(digit), .segments(led_out));
+    assign io_out[11] = 0; // this value isn't needed. 
 
 endmodule
